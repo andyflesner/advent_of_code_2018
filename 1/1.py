042061980a55d120f652cdee214dea503cf0ffa9
Andy Flesner <andy.flesner@quest.com>
2018-12-02 10:42:12 -0600

    completed day 1

---
+++ 1/1.py
@@ -0,0 +1,35 @@
+freq = [0]
+mods = []
+duplicate = False
+dupeint = 0
+part1 = 0
+
+input = open('input')
+for line in input:
+    mods.append(line.rstrip())
+input.close()
+
+while duplicate == False:
+    for mod in mods:
+        if mod[:1] == '+':
+            newfreq = freq[-1] + int(mod[1:])
+            #print(line.rstrip() + ' = ' + str(freq))
+        elif mod[:1] == '-':
+            newfreq = freq[-1] - int(mod[1:])
+            #print(line.rstrip() + ' = ' + str(freq))
+        else:
+            print('ERROR')
+            exit
+
+        freq.append(newfreq)
+
+        if freq.count(newfreq) > 1:
+            duplicate = True
+            dupeint = freq[-1]
+            break
+
+        if len(freq) == (len(mods) + 1):
+            part1 = freq[-1]
+    
+print('Ending Frequency (Part 1): ' + str(part1))
+print('First Duplicate Frequency (Part 2): ' + str(dupeint))
